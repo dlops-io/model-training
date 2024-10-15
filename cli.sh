8def62d1a72217fded0e932beb92a68895606855
shivasj <shivasj@gmail.com>
2024-10-15 13:41:01 -0400

    updated cli

--- cli.sh
+++ cli.sh
@@ -13,23 +13,25 @@ export ACCELERATOR_TYPE="NVIDIA_TESLA_T4"
 export ACCELERATOR_COUNT=1
 export GCP_REGION="us-central1" # Adjust region based on you approved quotas for GPUs
 
-#export CMDARGS="--model_name=mobilenetv2,--epochs=30,--batch_size=32,--wandb_key=$WANDB_KEY"
-#export CMDARGS="--model_name=mobilenetv2,--train_base,--epochs=30,--batch_size=32,--wandb_key=$WANDB_KEY"
-export CMDARGS="--model_name=mobilenetv2,--train_base,--epochs=30,--batch_size=32,--wandb_key=$WANDB_KEY"
+# Change the number of epochs
+#export CMDARGS="--model_name=mobilenetv2,--epochs=2,--batch_size=32,--wandb_key=$WANDB_KEY"
+#export CMDARGS="--model_name=mobilenetv2,--train_base,--epochs=2,--batch_size=32,--wandb_key=$WANDB_KEY"
+export CMDARGS="--model_name=mobilenetv2,--train_base,--epochs=2,--batch_size=32,--wandb_key=$WANDB_KEY"
 
-# gcloud ai custom-jobs create \
-#   --region=$GCP_REGION \
-#   --display-name=$DISPLAY_NAME \
-#   --python-package-uris=$PYTHON_PACKAGE_URI \
-#   --worker-pool-spec=machine-type=$MACHINE_TYPE,replica-count=$REPLICA_COUNT,accelerator-type=$ACCELERATOR_TYPE,accelerator-count=$ACCELERATOR_COUNT,executor-image-uri=$EXECUTOR_IMAGE_URI,python-module=$PYTHON_MODULE \
-#   --args=$CMDARGS
-
-
-# Run training with No GPU
-export EXECUTOR_IMAGE_URI="us-docker.pkg.dev/vertex-ai/training/tf-cpu.2-14.py310:latest"
+# Run training with GPU
 gcloud ai custom-jobs create \
   --region=$GCP_REGION \
   --display-name=$DISPLAY_NAME \
   --python-package-uris=$PYTHON_PACKAGE_URI \
-  --worker-pool-spec=machine-type=$MACHINE_TYPE,replica-count=$REPLICA_COUNT,executor-image-uri=$EXECUTOR_IMAGE_URI,python-module=$PYTHON_MODULE \
+  --worker-pool-spec=machine-type=$MACHINE_TYPE,replica-count=$REPLICA_COUNT,accelerator-type=$ACCELERATOR_TYPE,accelerator-count=$ACCELERATOR_COUNT,executor-image-uri=$EXECUTOR_IMAGE_URI,python-module=$PYTHON_MODULE \
   --args=$CMDARGS
+
+
+# Run training with No GPU
+# export EXECUTOR_IMAGE_URI="us-docker.pkg.dev/vertex-ai/training/tf-cpu.2-14.py310:latest"
+# gcloud ai custom-jobs create \
+#   --region=$GCP_REGION \
+#   --display-name=$DISPLAY_NAME \
+#   --python-package-uris=$PYTHON_PACKAGE_URI \
+#   --worker-pool-spec=machine-type=$MACHINE_TYPE,replica-count=$REPLICA_COUNT,executor-image-uri=$EXECUTOR_IMAGE_URI,python-module=$PYTHON_MODULE \
+#   --args=$CMDARGS
